5e2be59bc9f6fe2320fd1f54e959d299ef2ead8b
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-07-28 03:07:29 +0600

    added blocks and bundle

---
+++ static/desktop.bundles/index/index.deps.js
@@ -0,0 +1,13 @@
+({
+	mustDeps: [],
+	shouldDeps: [
+		{ block: 'page' },
+		{ block: 'header' },
+		{ block: 'logo' },
+		{ block: 'image', mods: { type: 'gulp-bem' }},
+		{ block: 'content' },
+		{ block: 'menu' },
+		{ block: 'menu-item' },
+		{ block: 'link' },
+	],
+})
