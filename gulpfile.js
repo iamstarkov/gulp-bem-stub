07b502c5cb634c561da2ed989181f5d2b2ddfc04
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-08-07 20:28:39 +0600

    draft

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp = require('gulp');
 
 var dest = 'dist';
+var bundle = 'desktop.bundles/beta';
+
+var levels = [
+	'static/libs/bem-core',
+	'static/libs/bem-components',
+	'static/desktop.blocks'
+];
+
+gulp.task('decl', function () {
+	var decl = bundle + '/' + '*.bemdecl.js';
+	gulp.src(decls)
+		.pipe(readDecl())
+		.pipe(readBlocks())
+		.pipe(flatten())
+
+});
 
 gulp.task('build', function () {
 
