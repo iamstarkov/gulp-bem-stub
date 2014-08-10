3638d2b6347cd7ba559abe727cbce87ded9fdbc9
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-10 15:24:08 +0600

    refactor(gulpfile): clean and consisten api
    More about this api draft can be found here - https://github.com/floatdrop/gulp-bem

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 var gulp = require('gulp');
-var bem = require('gulp-bem')
+var bem = require('gulp-bem');
 
-var pjoin = require('path').join;
-var paths = {
-	pages: 'desktop.bundles'
-}
+var join = require('path').join;
+var concat = require('gulp-concat');
 
 var levels = [
-	'static/libs/bem-core',
-	'static/libs/bem-components',
-	'static/desktop.blocks'
+    'static/libs/bem-core',
+    'static/libs/bem-components',
+    'static/desktop.blocks',
+    'static/desktop.bundles'
 ];
 
+function allDeps(level) { return join(level, '**/*.deps.js'); }
+
 gulp.task('build', function () {
-	gulp.src('static/**/*.deps.js')
-		.pipe(depsGraph(levels))
-		.pipe(depsByPage(pjoin(pages, 'index'))
-		.pipe(fork(
-			bem.css('[BEM].css')
-				.pipe(concat())
-				.dest('[BUNDLE].css'),
-			bem.js('[BEM].js')
-				.pipe(concat())
-				.dest('[BUNDLE].js'),
-		))
+    var tree = gulp.src(levels.map(allDeps)).pipe(bem.tree());
+    var deps = tree.deps('static/desktop.bundles/index');
+
+    deps.src('{block}.css')
+        .pipe(concat('index.css'))
+        .pipe(gulp.dest('./dist'));
+
+    deps.src('{block}.js')
+        .pipe(concat('index.js'))
+        .pipe(gulp.dest('./dist'));
 });
