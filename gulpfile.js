cc0ff1aba5e1623f7d5cb94d190c3994c16de5b3
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-12 15:11:59 +0600

    refactor(api): change gulp.src to bem.blocks

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,15 @@
 var gulp = require('gulp');
 var bem = require('gulp-bem');
-
-var join = require('path').join;
 var concat = require('gulp-concat');
 
 var levels = [
-	/*
-    'static/libs/bem-core',
-    'static/libs/bem-components',
-	*/
     'static/desktop.blocks',
     'static/desktop.bundles'
 ];
 
-function allDeps(level) { return join(level, '**/*.deps.js'); }
-
 gulp.task('build', function () {
-    var tree = gulp.src(levels.map(allDeps)).pipe(bem.tree());
-    var deps = tree.deps('static/desktop.bundles/index');
+    var tree = bem.blocks('static', levels).pipe(bem.tree());
+    var deps = tree.deps('desktop.bundles/index');
 
     deps.src('{block}.css')
         .pipe(concat('index.css'))
