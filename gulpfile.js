dc16ed9bd2814cc3b48a357336c0b0f44090d25f
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-16 14:31:44 +0600

    fix(gulpfile): remove bem-core and bem-components levels

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,25 @@ var concat = require('gulp-concat');
 
 var levels = [
     // bem-core
-    'libs/bem-core/common.blocks',
-    'libs/bem-core/desktop.blocks',
+    // 'libs/bem-core/common.blocks',
+    // 'libs/bem-core/desktop.blocks',
     // bem-components
-    'libs/bem-components/common.blocks',
-    'libs/bem-components/desktop.blocks',
+    // 'libs/bem-components/common.blocks',
+    // 'libs/bem-components/desktop.blocks',
     // local
     'desktop.blocks',
     'desktop.bundles/index'
 ];
 
-gulp.task('build', function () {
+gulp.task('build', function (cb) {
     var tree = bem.objects(levels).pipe(bem.tree());
     var deps = tree.deps('desktop.bundles/index/sepulka');
 
-    deps.src('{bem}.css')
+    return deps.pipe(bem.src('{bem}.css'))
         .pipe(concat('index.css'))
         .pipe(gulp.dest('./dist'));
 
-    deps.src('{bem}.js')
-        .pipe(concat('index.js'))
-        .pipe(gulp.dest('./dist'));
+    // deps.pipe(bem.src('{bem}.js'))
+    //     .pipe(concat('index.js'))
+    //     .pipe(gulp.dest('./dist'));
 });
