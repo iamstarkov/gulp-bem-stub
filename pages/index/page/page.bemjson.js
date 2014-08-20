755eea681ec9460d13736356474585aa89b5b531
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-08-20 20:43:50 +0600

    refactor(blocks): finish all the blocks!

--- pages/index/page/page.bemjson.js
+++ pages/index/page/page.bemjson.js
@@ -1,27 +1,49 @@
 module.exports = {
     block: 'page',
-    title: 'Gulp BEM is awesome!',
-    head: [
-        { elem: 'css', url: 'index.css' }
-    ],
-    scripts: [{ elem: 'js', url: 'index.js' }],
     content: [
-        { block: 'logo' },
         {
-            block: 'menu',
-            content: ['overview', 'examples', 'docs', 'bem'].map(function (item) {
-                return {
-                    elem: 'item',
-                    content: {
-                        block: 'link',
-                        tag: 'a',
-                        attrs: {
-                            href: '#' + item
-                        },
-                        content: item
-                    }
-                };
-            })
+            block: 'head',
+            tag: 'head',
+            content: [
+                {
+                    block: 'title',
+                    tag: 'title',
+                    content: 'Gulp BEM is awesome!',
+                },
+                {
+                    block: 'css',
+                    tag: 'link',
+                    attrs: { href: 'index.css', rel: 'stylesheet' }
+                }
+            ]
+        },
+        {
+            block: 'body',
+            tag: 'body',
+            content: [
+                { block: 'logo' },
+                {
+                    block: 'menu',
+                    content: ['overview', 'examples', 'docs', 'bem'].map(function (item) {
+                        return {
+                            elem: 'item',
+                            content: {
+                                block: 'link',
+                                tag: 'a',
+                                attrs: {
+                                    href: '#' + item
+                                },
+                                content: item
+                            }
+                        };
+                    })
+                },
+                {
+                    block: 'js',
+                    tag: 'script',
+                    attrs: { src: 'index.js' }
+                }
+            ]
         }
     ]
 };
