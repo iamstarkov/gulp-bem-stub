d94a3712f5b2ffcce6acacf9aa35d60d816576ed
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-08-23 20:06:07 +0600

    refactor(css): ger rid off redundant bem(false)

--- base/css/css.bh.js
+++ base/css/css.bh.js
@@ -1,16 +1,9 @@
 module.exports = function(bh) {
     bh.match('css', function(ctx, json) {
 
-        ctx.bem(false);
-
-        if (json.url) {
-            ctx
-                .tag('link')
-                .attr('rel', 'stylesheet')
-                .attr('href', json.url);
-        } else {
-            ctx.tag('style');
-        }
+        ctx.tag('link')
+            .attr('rel', 'stylesheet')
+            .attr('href', json.url);
 
     });
 };
