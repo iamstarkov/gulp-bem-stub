b76379bb6a2f4e19ecdf243e12a46d3e1b2f38e4
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-08-26 02:49:49 +0400

    refactor(index): move "bootstrap" depency to "require" section

--- pages/index/page/page.deps.js
+++ pages/index/page/page.deps.js
@@ -1,6 +1,8 @@
-module.exports.expect = [
+module.exports.require = [
     { block: 'bootstrap' },
+];
 
+module.exports.expect = [
     { block: 'logo' },
     { block: 'tagline' },
     { block: 'menu' },
